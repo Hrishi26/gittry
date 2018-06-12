03bae89aa23ed3c419af51ced8ccbe9f26f772be
Hrishi26 <shedekarhrishi@gmail.com>
2018-06-13 01:12:43 +0530

    Arduino codes upto 12 june 2018

---
+++ fishbot_nano/multi_servo1/multi_servo1.ino
@@ -0,0 +1,39 @@
+#include <Servo.h>
+
+Servo servo1; 
+Servo servo2;
+Servo servo3;
+Servo servo4;
+
+int i = 0;
+
+void setup() {
+  servo1.attach(4);
+  servo2.attach(5); 
+  servo3.attach(6); 
+  servo4.attach(7); 
+}
+
+void loop() {
+  /*
+  for (i = 0; i < 180; i++) { 
+    servo1.write(i);              
+    servo2.write(i);     
+    servo3.write(i);
+    servo4.write(i);         
+    delay(10);                      
+  }
+  for (i = 180; i > 0; i--) { 
+    servo1.write(i);                
+    servo2.write(i);     
+    servo3.write(i);
+    servo4.write(i);          
+    delay(10);                      
+  }
+  */
+  servo4.write(60);
+  delay(250);
+  servo4.write(140);
+  delay(250);
+
+}
